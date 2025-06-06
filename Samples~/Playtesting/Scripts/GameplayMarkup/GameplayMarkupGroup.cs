d2b9f01ce007f1c440bbf2c833b2fc15e7b25383
Nikolai Kochergin <Kocherginizer@gmail.com>
2025-06-06 11:42:20 +0300

    Add background music for test

--- Samples~/Playtesting/Scripts/GameplayMarkup/GameplayMarkupGroup.cs
+++ Samples~/Playtesting/Scripts/GameplayMarkup/GameplayMarkupGroup.cs
@@ -6,6 +6,8 @@ namespace VervePlace.YandexGames.Samples
     public class GameplayMarkupGroup : MonoBehaviour
     {
         [SerializeField] private TMP_Text _pauseStatusText;
+        [SerializeField] private TMP_Text _musicButtonText;
+        [SerializeField] private AudioSource _audioSource;
         
         private void Awake() => 
             GameplayMarkup.GamePaused += OnGamePaused;
@@ -15,10 +17,31 @@ namespace VervePlace.YandexGames.Samples
 
         public void OnGameStartButtonClicked() => GameplayMarkup.GameStart();
         public void OnGameStopButtonClicked() => GameplayMarkup.GameStop();
+
+        public void OnMusicButtonClicked()
+        {
+            if (_audioSource.isPlaying)
+            {
+                _audioSource.Stop();
+                _musicButtonText.text = "Play Music";
+            }
+            else
+            {
+                _audioSource.Play();
+                _musicButtonText.text = "Stop Music";
+            }
+        }
         
-        private void OnGamePaused(bool isPaused) =>
+        private void OnGamePaused(bool isPaused)
+        {
             _pauseStatusText.text = isPaused
                 ? "<color=\"red\">Paused"
                 : "<color=\"green\">Playing";
+            
+            if (isPaused)
+                _audioSource.Pause();
+            else
+                _audioSource.UnPause();
+        }
     }
 }
